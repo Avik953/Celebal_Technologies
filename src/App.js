d59c824598fb72c2e2434e13f6c9b774ccdcaa6c
Avik <aviksaha202002@gmail.com>
2023-07-06 16:00:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { red } from "@mui/material/colors";
+import React,{ useState } from "react";
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    alert(' Submitted:'+ "\n Name: "+name+"\n Email: "+email+"\n Phone Number: "+number+"\n Message: "+message
+    )
+  };
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography gutterBottom variant="h3" align="center" style={{padding:"20px 10px 0 0", color:"red"}}>
+        Celebal Technologies
+      </Typography>
+      <Card style={{maxWidth: 450, margin:"0 auto", padding:"20px 5px"}}>
+        <CardContent>
+        <Typography gutterBottom variant="h5">Contact Us</Typography>
+         <form onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid xs={12} item>
+              <TextField
+                label="Name"
+                placeholder="Enter your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="email"
+                label="Email"
+                placeholder="Enter your email id"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="number"
+                label="Phone Number"
+                placeholder="Enter your phone number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                label="Message"
+                multiline
+                rows={5}
+                placeholder="Type your message here"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                fullWidth
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Button type="submit" variant="contained" color="primary">
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
